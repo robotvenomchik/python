a7cab735bad6987fdabef15d1d411d93ec7d73cd
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2023-07-19 18:35:35 +0300

    .

--- homework1/test_home_lib.py
+++ homework1/test_home_lib.py
@@ -6,8 +6,8 @@ test_cases=[
     ("Ab2!",False),
     ("sadsad!12",False),
     ("FBSFS!23",False),
-    ("2145",False),
-    ("пепа",False),
+    ("2145!",False),
+    ("пепа!",False),
 ]
 @pytest.mark.parametrize('password, expected', test_cases)
 def test_is_password_suituble(password,expected):
